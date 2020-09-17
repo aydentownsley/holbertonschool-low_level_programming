ebb0b3f4ff3ddaf6da1b95509c15649b458253af
aydentownsley <aydentownsley@gmail.com>
2020-09-17 23:23:42 +0000

    fixing betty requirement in problem 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,13 +17,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (nth > 5)
-		printf("Last digit of %d is %d and is greater than 5"\
-		, n, nth);
+		printf("Last digit of %d is %d and is greater than 5", n, nth);
 	if (nth == 0)
 		printf("Last digit of %d is %d and is 0", n, nth);
 	if (nth < 6 && nth != 0)
-		printf("Last digit of %d is %d and is less than 6\
-		and not 0", n, nth);
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, nth);
 
 	printf("\n");
 
