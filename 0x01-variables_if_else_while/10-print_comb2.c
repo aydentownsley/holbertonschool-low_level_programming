e93b168bf5d216865f0a3587505e429f63429b7b
aydentownsley <aydentownsley@gmail.com>
2020-09-18 03:04:53 +0000

    adding final mandatory problems

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints all combos of double digits numbers
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 48; m < 58; m++)
+		{
+			putchar(n);
+			putchar(m);
+			if(n == 57 && m == 57)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
