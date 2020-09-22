25369b961b2b33ecae20818e4138ff4e9e513a4f
aydentownsley <aydentownsley@gmail.com>
2020-09-22 23:46:32 +0000

    fixed a extra space in 9, times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,19 +19,26 @@ void times_table(void)
 			tens = hold / 10;
 			ones = hold % 10;
 
-			if (tens == 0)
+			if (tens == 0 && ones == 0)
+			{
+				_putchar(ones + 48);
+			} else if (tens == 0)
 			{
 				_putchar(32);
 				_putchar(ones + 48);
 			} else
 			{
 				_putchar(tens + 48);
-				_putchar(ones + 48);
+                                _putchar(ones + 48);
 			}
 
 			if (m < 9)
 			{
 				_putchar(44);
+				_putchar(32);
+			}
+			if (n == 0 && m != 9)
+			{
 				_putchar(32);
 			}
 		}
