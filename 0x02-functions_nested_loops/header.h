c14c02990a1bd32eb92a949d34ee9a2ec254b063
aydentownsley <aydentownsley@gmail.com>
2020-09-22 15:13:51 +0000

    fixed header file to satisfy betty

--- 0x02-functions_nested_loops/header.h
+++ 0x02-functions_nested_loops/header.h
@@ -1,2 +1,10 @@
-void _putchar();
-void print_alphabet();
+#ifndef _HEADER_H_
+#define _HEADER_H_
+
+/* this prototype has to be declated in a header file */
+int _putchar(char);
+
+/* this prototype has to be declated in a header file */
+void print_alphabet(void);
+
+#endif /* _HEADER_H_ */
