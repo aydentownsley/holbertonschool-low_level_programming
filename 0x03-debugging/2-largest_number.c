92862938c7f57ca489ad7327d590f9c6d7c5a82b
aydentownsley <aydentownsley@gmail.com>
2020-09-23 02:00:44 +0000

    added other cases to largest numeber to handle identical numbers

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -20,10 +20,25 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else
+	else if (c > a && c > b)
 	{
 		largest = c;
+	} 
+	else if (a == b && a > c)
+	{
+		largest = a;
 	}
-
+	else if (a == c && a > b)
+	{
+		largest = a;
+	}
+	else if (b == c && b > a)
+	{
+		largest = b;
+	}
+	else if (a == b && a == c && b == c)
+	{
+		largest = a;
+	}
 	return (largest);
 }
