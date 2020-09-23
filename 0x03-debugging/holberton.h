72351cf34e024cfdd47ea3c5981683027475ebc9
aydentownsley <aydentownsley@gmail.com>
2020-09-23 02:31:23 +0000

    fixed leap year math in the if statement

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -2,8 +2,12 @@
 #define HOLBERTON_H
 
 #include <stdio.h>
+/** this is a prototype function */
+void positive_or_negative(int);
 
-/** this is the prototype function */
+/** this is a prototype function */
 int largest_number(int, int, int);
 
+/** this is a prototype function */
+void print_remaining_days(int, int, int);
 #endif /* HOLBERTON_H */
