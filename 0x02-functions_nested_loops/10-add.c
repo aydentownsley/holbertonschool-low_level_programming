4383ba5896b435ff1d83aa8221c49c1f46e0ee0c
aydentownsley <aydentownsley@gmail.com>
2020-09-23 04:18:29 +0000

    adding project 10, add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * add - adds two ints
+ *
+ * Return: sum of two ints
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,33 +1,36 @@
 #ifndef _HEADER_H_
 #define _HEADER_H_
 
-/* this prototype has to be declated in a header file */
+/* this prototype has to be declared in a header file */
 int _putchar(char);
 
-/* this prototype has to be declated in a header file */
+/* this prototype has to be declared in a header file */
 void print_alphabet(void);
 
-/* this prototype has to be declated in a header file */
+/* this prototype has to be declared in a header file */
 void print_alphabet_x10(void);
 
-/* this prototype has to be declated in a header file */
+/* this prototype has to be declared in a header file */
 int _islower(int c);
 
-/* this prototype has to be declated in a header file */
+/* this prototype has to be declared in a header file */
 int _isalpha(int c);
 
-/* this prototype has to be declated in a header file */
+/* this prototype has to be declared in a header file */
 int print_sign(int n);
 
-/* this prototype has to be declated in a header file */
+/* this prototype has to be declared in a header file */
 int _abs(int);
 
-/* this prototype has to be declated in a header file */
+/* this prototype has to be declared in a header file */
 int print_last_digit(int);
 
-/* this prototype has to be declated in a header file */
+/* this prototype has to be declared in a header file */
 void jack_bauer(void);
 
-/* this prototype has to be declated in a header file */
+/* this prototype has to be declared in a header file */
 void times_table(void);
+
+/** this prototype has to be declared in a header file */
+int add(int, int);
 #endif /* _HEADER_H_ */
