e8f412ac6524420af0feee3cbf107553bac6dd0b
aydentownsley <aydentownsley@gmail.com>
2020-10-02 01:16:07 +0000

    changed 3 to not just compare length

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,7 +11,7 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int len1 = 0, len2 = 0;
+	int len1 = 0, len2 = 0, i = 0;
 
 	while (s1[len1] != '\0')
 	{
@@ -23,11 +23,11 @@ int _strcmp(char *s1, char *s2)
 		len2++;
 	}
 
-	if (len1 == len2)
+	if (s1[i] == s2[i] && len1 == len2)
 		return (0);
 	else if (len1 > len2)
-		return (15);
+		return (1);
 	else
-		return (-15);
+		return (-1);
 
 }
