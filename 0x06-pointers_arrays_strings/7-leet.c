2889d2fc5f46c0228999d7d86cbd337d59e73cb9
aydentownsley <aydentownsley@gmail.com>
2020-10-02 15:42:45 +0000

    fixed project 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,7 +12,7 @@ char *leet(char *s)
 {
 	int i = 0;
 	int n = 0;
-	char lt[] = "a4A4e3E3o0O0t7T7l1L1"
+	char lt[] = "a4A4e3E3o0O0t7T7l1L1";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
@@ -20,8 +20,9 @@ char *leet(char *s)
 		{
 			if (s[i] == lt[n])
 			{
-				
+				s[i] = lt[n + 1];
 			}
+			n++;
 		}
 	}
 
