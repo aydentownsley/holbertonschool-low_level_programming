04adee0022bfb9b41bf2cc3e0042e3222f3ff928
aydentownsley <aydentownsley@gmail.com>
2020-10-05 18:01:21 +0000

    first commit, adding file 0

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+## README
+
+# 0x07. C - Even more pointers, arrays and strings
+
+This directory containes project files, test files, and executables for project 0x07.
---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,10 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/* This is a prototype function of putchar */
+int _putchar(char);
+
+/* This is a prototype of the function _memset */
+char *_memset(char *, char, unsigned int);
+
+#endif /*_HOLBERTON_H_*/
