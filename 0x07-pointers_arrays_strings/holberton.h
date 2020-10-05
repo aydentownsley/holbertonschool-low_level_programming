3a0867f9ede56bc0dd7359e1cbfabef78fd8535c
aydentownsley <aydentownsley@gmail.com>
2020-10-05 18:16:00 +0000

    adding project file 1

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -7,4 +7,7 @@ int _putchar(char);
 /* This is a prototype of the function _memset */
 char *_memset(char *, char, unsigned int);
 
+/* This is a protoype of the function _memcpy */
+char *_memcpy(char *, char *, unsigned int);
+
 #endif /*_HOLBERTON_H_*/
