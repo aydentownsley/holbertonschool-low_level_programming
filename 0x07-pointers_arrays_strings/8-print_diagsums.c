48f887e7af5933792d25dc11450ca6bbe6a21d10
aydentownsley <aydentownsley@gmail.com>
2020-10-06 19:09:01 +0000

    changed sturcture of diagsums to for loops for clearer understanding

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,30 +1,20 @@
 #include "holberton.h"
 #include <stdio.h>
 
-/**
- * print_diagsums - print diagonal sums of a square matrix
- *
- * @a: pointer to 2D array
- * @size: size of the array
- *
- * Return: void
- */
-
 void print_diagsums(int *a, int size)
 {
-	int l = 0, w = 0, asum = 0, bsum = 0;
+	int d1 = 0, d2 = 0, asum = 0, bsum = 0;
 
-	while (l <= (size * size))
+	for (d1 = 0; d1 < (size * size); d1 += (size + 1))
 	{
-		asum = asum + a[l];
-		l = l + size + 1;
+		asum += a[d1];
 	}
 
-	while (w < (size * size - 1))
+	for (d2 = 0; d2 < (size * size) - 1; d2 += (size - 1))
 	{
-		bsum = bsum + a[w];
-		w = w + size - 1;
+		bsum += a[d2];
 	}
 
 	printf("%d, %d\n", asum, bsum);
+
 }
---
+++ 0x07-pointers_arrays_strings/testfiles/8-print_diagsums.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - print diagonal sums of a square matrix
+ *
+ * @a: pointer to 2D array
+ * @size: size of the array
+ *
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int l = 0, w = 0, asum = 0, bsum = 0;
+
+	while (l <= (size * size))
+	{
+		asum = asum + a[l];
+		l = l + size + 1;
+	}
+
+	while (w < (size * size - 1))
+	{
+		bsum = bsum + a[w];
+		w = w + size - 1;
+	}
+
+	printf("%d, %d\n", asum, bsum);
+}
