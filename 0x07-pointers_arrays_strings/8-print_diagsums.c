f91578a98804b8dd58821693a4d55ab8626a6152
aydentownsley <aydentownsley@gmail.com>
2020-10-06 19:30:36 +0000

    fixed diagsums, was setting d2 at two points

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,16 +1,25 @@
 #include "holberton.h"
 #include <stdio.h>
 
+/**
+ * print_diagsums - print the two diagonal sums of an n size 2d array
+ *
+ * @a: pointer to 2D array
+ * @size: size of array
+ *
+ * Return: void
+*/
+
 void print_diagsums(int *a, int size)
 {
-	int d1 = 0, d2 = 0, asum = 0, bsum = 0;
+	int d1, d2, asum = 0, bsum = 0;
 
 	for (d1 = 0; d1 < (size * size); d1 += (size + 1))
 	{
 		asum += a[d1];
 	}
 
-	for (d2 = 0; d2 < (size * size) - 1; d2 += (size - 1))
+	for (d2 = size - 1; d2 < (size * size) - 2; d2 += (size - 1))
 	{
 		bsum += a[d2];
 	}
