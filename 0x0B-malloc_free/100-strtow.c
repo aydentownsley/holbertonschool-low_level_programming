53b15ebc03aaf8eeff634bef25f55328d15ea941
aydentownsley <aydentownsley@gmail.com>
2020-10-14 04:48:04 +0000

    adding what I have of 100

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -13,5 +13,18 @@
 
 char **strtow(char *str)
 {
-	
+	char *new;
+	int a;
+	int sz;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+
+	for (sz = 0; str[sz] != '\0'; sz++)
+		;
+
+	/* sz + 2 is to accomodate for newline and escape char */
+	new = (char *)malloc((a + 2) * sizeof(char));
+
+	return (new);
 }
