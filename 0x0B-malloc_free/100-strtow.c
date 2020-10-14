9c7b4ddddfcb830ab018408584f7c53b3d0b4abe
aydentownsley <aydentownsley@gmail.com>
2020-10-14 04:23:29 +0000

    adding 6 file

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * strtow - split a string into words
+ *
+ * @str: string to be split
+ *
+ * Return: POINTER to a new array of "words"
+ * OR NULL if str is NULL or empty
+ * OR NULL if fail
+ */
+
+char **strtow(char *str)
+{
+	
+}
