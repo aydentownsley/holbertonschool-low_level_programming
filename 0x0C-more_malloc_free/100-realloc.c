b86798898a8143a2dd0e94a284229bf92738c93c
aydentownsley <aydentownsley@gmail.com>
2020-10-16 19:39:24 +0000

    added memcpy into realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,6 +3,28 @@
 #include "holberton.h"
 
 /**
+ * _memcpy - copies n bytes of one array to another
+ *
+ * @src: array to be copied from
+ * @dest: array to be copied to
+ * @n: number of elements to be copied
+ *
+ * Return: dest pointer
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+
+/**
  * _realloc - reallocates memory
  *
  * @ptr: pointer to previously allocated array
