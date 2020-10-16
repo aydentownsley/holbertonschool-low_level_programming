b4690d68be746e98e11f83ed2e1ed842e60a7be9
aydentownsley <aydentownsley@gmail.com>
2020-10-16 00:28:36 +0000

    was one short on mem alloc when n >= 2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -44,7 +44,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 
-	if (n > len2)
+	if (n >= len2)
 		p = malloc((sizeof(*p) * (len1 + len2)));
 	else
 		p = malloc((sizeof(*p) * (len1 + n + 1)));
