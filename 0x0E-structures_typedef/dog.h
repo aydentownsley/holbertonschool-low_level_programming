26ea70d86e07b1251260fd311cf7940a8b020f60
aydentownsley <aydentownsley@gmail.com>
2020-10-19 19:18:44 +0000

    forgot my description of dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct Dog - creates a structre of type dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ *
+ * Description: creates a dog type, that has name, owner,
+ * and age values.
+ */
+
 struct Dog
 {
 	char *name;
