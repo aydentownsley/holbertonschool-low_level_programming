92d5029943334c26c8fe40ee89e149eef08dd086
aydentownsley <aydentownsley@gmail.com>
2020-10-19 21:39:47 +0000

    I dont want to exit, so I changed to do nothing if NULL

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -15,9 +15,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		exit(100);	
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
