f5cc00248325ff09f80633f57fab3bf5793da479
aydentownsley <aydentownsley@gmail.com>
2020-10-21 23:03:07 +0000

    adding files for 0x0F

---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,6 @@
+# READ ME
+
+# 0x0F. C - Function pointers
+
+This repository contains files written in C for the function pointers project.
+The goal is to fully understand the use and abilities of function pointers.
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
+
+/* Prototype of _putchar */
+int _putchar(char);
+
+/* Protorype of print_name */
+void print_name(char *, void (*f)(char *));
+
+#endif /* _FUNCTION_POINTERS_H_ */
