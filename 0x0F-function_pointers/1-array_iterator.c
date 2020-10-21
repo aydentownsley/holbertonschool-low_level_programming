678d7d8541e120bd1649ee8cad212aeb09e0ac85
aydentownsley <aydentownsley@gmail.com>
2020-10-21 23:27:58 +0000

    I was using array[size], needed to iterate with array[i], with i < size

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,10 +2,23 @@
 #include <stdlib.h>
 #include "function_pointers.h"
 
+/**
+ * array_iterator - executes function pointed to
+ *
+ * @array: passed array
+ * @size: size of passed array
+ * @action: funtion pointer
+ *
+ * Return: void
+ */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	unsigned int i;
+
 	if (action == NULL || array == NULL)
 		return;
 
-	action(array[size]);
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
