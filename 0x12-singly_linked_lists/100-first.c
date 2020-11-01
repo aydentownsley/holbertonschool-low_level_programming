2e4e7781b7480f6921f221d42dc65858f78658f1
aydentownsley <aydentownsley@gmail.com>
2020-11-01 02:08:47 +0000

    adding 100 advanced task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+void before_main(void) __attribute__((constructor));
+
+/**
+ * before_main - prints a statement before main executes
+ *
+ * Return: void
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
