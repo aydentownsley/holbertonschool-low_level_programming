238f7394a8be230d43a8f69d25d80711a3f1e8f4
aydentownsley <aydentownsley@gmail.com>
2020-11-01 02:59:18 +0000

    learned this in a later project applying it here

---
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,14 @@
+    global    main
+    section   .text
+main:
+    mov       rax, 1
+    mov       rdi, 1
+    mov       rsi, message
+    mov       rdx, 17
+    syscall
+    mov       rax, 0x02000001
+    xor       rdi, rdi
+    syscall
+    section   .data
+message:
+    db        "Hello, Holberton", 10
