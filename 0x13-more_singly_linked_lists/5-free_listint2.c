fb62d790a252afe835af032dea1fd0c5fe7b4f57
aydentownsley <aydentownsley@gmail.com>
2020-11-05 01:17:06 +0000

    adding 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listin2 - frees a list
+ *
+ * @head: head of list
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *curr, *next;
+
+	curr = *head;
+
+	while (curr)
+	{
+		next = curr->next;
+		free(curr);
+		curr = next;
+	}
+
+	*head = NULL;
+}
