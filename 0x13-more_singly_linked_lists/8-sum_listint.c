d48d1c7d6145c42b769f4a24498aec0f3b8d1a51
aydentownsley <aydentownsley@gmail.com>
2020-11-05 03:50:22 +0000

    adding 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * sum_listint - returns sum of data in all nodes of list
+ *
+ * @head: head of list
+ *
+ * Return: SUM of data in nodes
+ * OR 0 if list is empty
+ */
+
+int sum_listint(listint_t *head)
+{
+	listint_t *current = head;
+	int sum = 0;
+
+	if (current == NULL)
+		return (0);
+
+	while (current)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+
+	return (sum);
+}
