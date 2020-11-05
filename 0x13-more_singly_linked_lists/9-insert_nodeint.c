c1c722e59e7f03b9d4293d43849688061f7b4751
aydentownsley <aydentownsley@gmail.com>
2020-11-05 05:44:55 +0000

    fixed indexing issue

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,7 +35,7 @@ unsigned int listlength(listint_t *h)
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new, *current = *head;
-	unsigned int itr = 0, listlen = 0;
+	unsigned int itr = 1, listlen = 0;
 
 	new = malloc(sizeof(listint_t));
 
