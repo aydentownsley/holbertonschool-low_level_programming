745a49a3f1b521b38e0dd8b39d843bef6663e0f4
aydentownsley <aydentownsley@gmail.com>
2020-11-05 04:39:32 +0000

    adding null check for current node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,6 +21,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 
+	if (current == NULL)
+		return (NULL);
+
 	while (current && itr < idx)
 	{
 		current = current->next;
