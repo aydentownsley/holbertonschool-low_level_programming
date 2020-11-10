9023c2b887463486994410b83857b2b53762f9bb
aydentownsley <aydentownsley@gmail.com>
2020-11-10 05:34:52 +0000

    adding elf file

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * main - find and print desired info from elf header
+ *
+ * @argv: array of args
+ * @argc: number of args
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *varg[])
+{
+	
+}
