410e8435232f8f4e5eefe747cc7d3ac8417adecb
aydentownsley <aydentownsley@gmail.com>
2020-11-10 05:28:40 +0000

    adding filname NULL case and deleting planning comments

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,31 +17,28 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	/* initialize variable */
 	int fd, wr, i, textlen = 0;
 
-	/* open file, if doesnt exist return -1 */
+	if (filename == NULL)
+		return (-1);
+
 	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
 		return (-1);
 
-	/* get textlen */
 	if (text_content == NULL)
 		return (1);
 
 	for (i = 0; text_content[i]; ++i, ++textlen)
 		;
 
-	/* append needed text, if text is NULL return 1 and close */
 	wr = write(fd, text_content, textlen);
 
 	if (wr == -1)
 		return (-1);
 	printf("write sucess\n");
-	/* close file */
 	close(fd);
 
-	/* return 1 if success */
 	return (1);
 }
