1449c504e88732f1cf6c93c58aa61d38d5f65c1e
aydentownsley <aydentownsley@gmail.com>
2020-12-13 19:40:22 +0000

    fixed 0, should fix 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,3 +1,4 @@
+
 #include "lists.h"
 
 /**
@@ -21,11 +22,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new->n = n;
 	new->prev = NULL;
-
 	new->next = *head;
-	if (*head != NULL)
-		(*head)->prev = new;
-	*head = new;
 
+	*head = new;
 	return (new);
 }
