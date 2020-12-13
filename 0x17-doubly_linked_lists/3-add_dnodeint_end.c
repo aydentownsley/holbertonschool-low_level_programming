f8a8c59ff13def5fbc1a05c48b7a6102ba792d5c
aydentownsley <aydentownsley@gmail.com>
2020-12-13 19:10:57 +0000

    making sure github is up to date for checker

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - adds a node to the end of a doubly linked list
+ *
+ * @head: head of the list
+ * @n: interger to store in new node
+ *
+ * Return: Address of new node
+ * OR NULL if failed to add
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+    dlistint_t *new, *temp;
+
+    new = malloc(sizeof(dlistint_t));
+
+    if (new == NULL)
+        return (NULL);
+
+    if (n == '\0')
+        return (NULL);
+    new->n = n;
+
+    new->next = NULL;
+
+    if (*head == NULL)
+    {
+        *head = new;
+    }
+    else
+    {
+        temp = *head;
+        while (temp->next != NULL)
+            temp = temp->next;
+
+        temp->next = new;
+    }
+
+    return (new);
+}
