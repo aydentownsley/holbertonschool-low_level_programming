d5bfbf99752d3b06c042fa1ab20cfc3b77f165de
aydentownsley <aydentownsley@gmail.com>
2020-12-13 20:48:16 +0000

    changed where i set prev value

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -52,8 +52,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	current = *h;
 	while (current && itr < idx)
 	{
-		current = current->next;
 		current->prev = current;
+		current = current->next;
 		++itr;
 	}
 	new->n = n;
