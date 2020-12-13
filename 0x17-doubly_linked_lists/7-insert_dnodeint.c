881de6fc68cc8507ba2ef221db1ce8dd67d1c5e7
aydentownsley <aydentownsley@gmail.com>
2020-12-13 20:25:39 +0000

    adding problem 7

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,77 @@
+#include "lists.h"
+
+/**
+ * listlength - gets length of linked list
+ *
+ * @h: head of list
+ *
+ * Return: length
+ */
+
+unsigned int listlength(dlistint_t *h)
+{
+	unsigned int length = 0;
+
+	while (h)
+	{
+		h = h->next;
+		++length;
+	}
+
+	return (length);
+}
+
+/**
+ * insert_dnodeint_at_index - inserts node into list at nth index
+ *
+ * @h: head of list
+ * @idx: index to insert node
+ * @n: value to put in inserted node
+ *
+ * Return: ADDRESS of the new node
+ * OR NULL if failed
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new, *current = *h;
+	unsigned int itr = 1, listlen = 0;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL || current == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	listlen = listlength(current);
+	if (idx >= listlen)
+	{
+		free(new);
+		return (NULL);
+	}
+	current = *h;
+
+	while (current && itr < idx)
+	{
+		current = current->next;
+		++itr;
+	}
+	if (n)
+		new->n = n;
+	else
+	{
+		free(new);
+		return (NULL);
+	}
+	if (idx != 0)
+	{
+		new->next = current->next;
+		current->next = new;
+	}
+	else
+	{
+		new->next = current;
+		*h = new;
+	}
+	return (new);
+}
