561b8db051988f55555fe7cd510bcdc7bb5f8bfc
aydentownsley <aydentownsley@gmail.com>
2020-12-13 19:42:34 +0000

    reverting 2, to changes made before 0 fix

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,4 +1,3 @@
-
 #include "lists.h"
 
 /**
@@ -22,8 +21,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new->n = n;
 	new->prev = NULL;
-	new->next = *head;
 
+	new->next = *head;
+	if (*head != NULL)
+		(*head)->prev = new;
 	*head = new;
+
 	return (new);
 }
