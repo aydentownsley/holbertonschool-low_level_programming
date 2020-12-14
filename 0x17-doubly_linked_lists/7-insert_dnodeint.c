f394ceb5a3cb507e78b17d0d6c98a2f739037471
aydentownsley <aydentownsley@gmail.com>
2020-12-14 06:23:00 +0000

    checking if head is null

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -37,6 +37,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *cursor = *h;
 	unsigned int itr = 1;
 
+	if (h == NULL)
+		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 
 	if (new == NULL)
