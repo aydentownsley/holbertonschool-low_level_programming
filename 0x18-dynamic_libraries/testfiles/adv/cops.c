861ded9b383a8852fcb2761ad53e3c4307b9f102
aydentownsley <aydentownsley@gmail.com>
2020-12-15 15:12:16 +0000

    updating directory to not have main in the main directory

---
+++ 0x18-dynamic_libraries/testfiles/adv/cops.c
@@ -0,0 +1,64 @@
+/**
+ * add - adds two ints
+ *
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: a + b
+ */
+int add(a, b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two ints
+ *
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: a - b
+ */
+int sub(a, b)
+{
+        return (a - b);
+}
+
+/**
+ * mul - multiplies two ints
+ *
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: a * b
+ */
+int mul(a, b)
+{
+        return (a * b);
+}
+
+/**
+ * div - divides two ints
+ *
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: a / b
+ */
+int div(a, b)
+{
+        return (a / b);
+}
+
+/**
+ * mod - modulos two ints
+ *
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: a % b
+ */
+int mod(a, b)
+{
+        return (a % b);
+}
