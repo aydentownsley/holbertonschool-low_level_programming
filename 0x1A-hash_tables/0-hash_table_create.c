63899fc3d875b2992d457de0cad368eaf0e4a742
aydentownsley <aydentownsley@gmail.com>
2021-01-15 00:09:31 +0000

    firt push

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ *
+ * @size: size of the hash table
+ * Return: pointer to created table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int x = 0;
+	hash_table_t *table;
+
+	table = malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+		return (NULL);
+
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t*) * size);
+
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+
+	}
+
+	for (; x < size; x++)
+		table->array[x] = NULL;
+
+	return (table);
+}
