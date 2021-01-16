0c600f533abca6576b94912f89996e29825c346b
aydentownsley <aydentownsley@gmail.com>
2021-01-16 03:12:23 +0000

    forgot a free for a new node check

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int counter = 0;
-	hash_node_t *new_node = NULL, *curr = NULL, **bin;
+	hash_node_t *new_node, *curr, **bin;
 
 	if (ht == NULL || key == NULL || strcmp(key, "") == 0 || value == NULL)
 		return (0);
@@ -35,6 +35,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (new_node == NULL)
 		return (0);
 	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+		free(new_node);
 	if (new_node->value == NULL)
 	{
 		free(new_node);
