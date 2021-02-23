533385b60377837116079e482f129bb5846c7605
aydentownsley <aydentownsley@gmail.com>
2021-02-23 19:12:00 +0000

    adding 2-5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Island Perimeter Module """
+
+def island_perimeter(grid):
+    """ Finds perimeter of an island """
+    p = 0
+    for l in range(len(grid)):
+        for w in range(len(grid[l])):
+            if grid[l][w] == 1:
+                if l != 0:
+                    if not grid[l - 1][w]:
+                        p += 1
+                else:
+                    p += 1
+                if l != len(grid) - 1:
+                    if not grid[l + 1][w]:
+                        p += 1
+                else:
+                    p += 1
+                if w != 0:
+                    if not grid[l][w - 1]:
+                        p += 1
+                else:
+                    p += 1
+                if w != len(grid[l]) - 1:
+                    if not grid[l][w + 1]:
+                        p += 1
+                else:
+                    p += 1
+    return p
