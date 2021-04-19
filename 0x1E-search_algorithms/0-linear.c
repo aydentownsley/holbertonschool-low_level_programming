724b0ae6d22f20ce95ea8126b4172cd298b3871c
aydentownsley <aydentownsley@gmail.com>
2021-04-19 18:40:23 +0000

    betty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -6,9 +6,9 @@
  * linear_search - checks each element of structure
  * until item is found/not found
  *
- * array - array to linearly search
- * size  - size of the array
- * value - element searched for
+ * @array: array to linearly search
+ * @size:  size of the array
+ * @value: element searched for
  *
  * Return: INDEX of value in array
  * OR NULL if not present in array
@@ -19,14 +19,14 @@ int linear_search(int *array, size_t size, int value)
 	size_t i = 0;
 
 	if (array == NULL || size == 0)
-		return -1;
+		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-			return i;
+			return (i);
 	}
 
-	return -1;
+	return (-1);
 }
